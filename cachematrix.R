66de0f9b56ae00c90b8f618da69dc82e9127af96
TyMaHbl4 <bgulyaev@gmail.com>
2014-05-23 23:09:22 +0300

    Updating my own CacheMatrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is so far assignment fulfill
+## which present two basic functions
 
-## Write a short comment describing this function
+## makeCacheMatrix will revert (inversing) matrix received as
+## input and returning inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+     inv <- NULL
+     set <- function(y){
+               x <<- y
+               inv <<- NULL ## here's a special assignment operator used 
 
+     }
+     get <- function() x  ## resolving back a function
+     setinv <- function(solve) inv <<- solve
+     getinv <- function() inv
+     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Next function will calculate the inverse of the special 
+## "matrix" returned by the previous function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)){
+             message("Calculating cached input") ## Just some  basic message to be shown 
+             return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setmean(solve(inv))
+        inv
 }
+
+m <- makeCacheMatrix(matrix(1:4, 2))
+cacheSolve(m)
+
+# This second call will use the cached matrix
+cacheSolve(m)
